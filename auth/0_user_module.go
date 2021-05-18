41f287771ad835014fa81a46c380bc41c38f01e1
ravielze <ravielz20@gmail.com>
2021-05-18 20:22:31 +0700

    Admin default

--- auth/0_user_module.go
+++ auth/0_user_module.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/ravielze/oculi/common"
 	"gorm.io/gorm"
 )
 
@@ -29,6 +32,18 @@ func NewModule(db *gorm.DB, g *gin.Engine) Module {
 	cont := NewController(g, uc)
 
 	db.AutoMigrate(&User{})
+
+	hashedPassword, err := Hash(os.Getenv("ADMIN_DEFAULT_PASSWORD"))
+	if err != nil {
+		panic(err)
+	}
+	db.FirstOrCreate(&User{
+		IntIDBase: common.IntIDBase{ID: 1},
+		Email:     os.Getenv("ADMIN_DEFAULT_EMAIL"),
+		Name:      "admin",
+		Password:  hashedPassword,
+		Role:      int16(ROLE_ADMIN),
+	})
 
 	return Module{
 		controller: cont,
