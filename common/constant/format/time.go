ae1df4ada3137c11b4297c3e19b337b53ba3eaaa
ravielze <ravielz20@gmail.com>
2021-06-19 03:17:56 +0700

    time format

---
+++ common/constant/format/time.go
@@ -0,0 +1,9 @@
+package format
+
+const (
+	DATE         = "02-01-2006"
+	DATE_DAY     = "Mon, 02 Jan 2006"
+	DATETIME     = "02-01-2006 15:04:05"
+	DATETIME_DAY = "Mon, 02 Jan 2006 15:04:05"
+	TIME         = "15:04:05"
+)
