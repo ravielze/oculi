f6699dcb2aaae211bf42ffe745b65e6b360a59a3
ravielze <ravielz20@gmail.com>
2021-07-12 00:12:18 +0700

    Refactor di

--- di/di.go
+++ di/di.go
@@ -10,41 +10,7 @@ import (
 type (
 	Registerable      func(c *dig.Container) error
 	ContainerFunction func(once *sync.Once, container *dig.Container) (*dig.Container, error)
-
-	Registrant struct {
-		container *dig.Container
-		err       error
-	}
 )
-
-func NewRegistrant(container *dig.Container) *Registrant {
-	return &Registrant{
-		container: container,
-		err:       nil,
-	}
-}
-
-func (r *Registrant) Provide(constructor interface{}, opts ...dig.ProvideOption) *Registrant {
-	if r.err == nil {
-		if err := r.container.Provide(constructor, opts...); err != nil {
-			r.err = errors.Wrap(err, "error on initialize")
-		}
-	}
-	return r
-}
-
-func (r *Registrant) Register(rf Registerable) *Registrant {
-	if r.err == nil {
-		if err := rf(r.container); err != nil {
-			r.err = err
-		}
-	}
-	return r
-}
-
-func (r *Registrant) End() error {
-	return r.err
-}
 
 func GenerateContainerFunction(items []Registerable) ContainerFunction {
 	return func(once *sync.Once, container *dig.Container) (*dig.Container, error) {
---
+++ di/registrant.go
@@ -0,0 +1,42 @@
+package di
+
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/dig"
+)
+
+type (
+	Registrant struct {
+		container *dig.Container
+		err       error
+	}
+)
+
+func NewRegistrant(container *dig.Container) *Registrant {
+	return &Registrant{
+		container: container,
+		err:       nil,
+	}
+}
+
+func (r *Registrant) Provide(constructor interface{}, opts ...dig.ProvideOption) *Registrant {
+	if r.err == nil {
+		if err := r.container.Provide(constructor, opts...); err != nil {
+			r.err = errors.Wrap(err, "error on initialize")
+		}
+	}
+	return r
+}
+
+func (r *Registrant) Register(rf Registerable) *Registrant {
+	if r.err == nil {
+		if err := rf(r.container); err != nil {
+			r.err = err
+		}
+	}
+	return r
+}
+
+func (r *Registrant) End() error {
+	return r.err
+}
