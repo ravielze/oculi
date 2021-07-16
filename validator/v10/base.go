70d40ba5367505c5f1702e22389fe255b5875bfd
ravielze <ravielz20@gmail.com>
2021-07-17 00:35:27 +0700

    validator

---
+++ validator/v10/base.go
@@ -0,0 +1,67 @@
+package v10
+
+import (
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/id"
+	ut "github.com/go-playground/universal-translator"
+	v10 "github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+	"github.com/ravielze/oculi/validator"
+)
+
+type (
+	impl struct {
+		instance *v10.Validate
+		trans    ut.Translator
+	}
+)
+
+func New() (validator.Validator, error) {
+	langEn := en.New()
+	langId := id.New()
+	uni := ut.New(langEn, langEn, langId)
+	trans, _ := uni.GetTranslator("en")
+
+	validate := v10.New()
+	if err := en_translations.RegisterDefaultTranslations(validate, trans); err != nil {
+		return nil, err
+	}
+
+	instance := &impl{instance: validate, trans: trans}
+	if err := instance.installDefaultValidator(); err != nil {
+		return nil, err
+	}
+	return instance, nil
+}
+
+func (i *impl) installDefaultValidator() error {
+	return nil
+}
+
+func (i *impl) Validate(object interface{}) error {
+	return i.instance.Struct(object)
+}
+
+func (i *impl) ValidateVar(obj interface{}, tag string) error {
+	return i.instance.Var(obj, tag)
+}
+
+func (i *impl) RegisterValidation(tag string, fn interface{}) {
+	if fnConv, ok := fn.(v10.Func); !ok {
+		panic("fn is not v10.Func")
+	} else {
+		i.instance.RegisterValidation(tag, fnConv)
+	}
+}
+
+func (i *impl) RegisterCustomTypeFunc(fn validator.CustomTypeFunc, types ...interface{}) {
+	i.instance.RegisterCustomTypeFunc(v10.CustomTypeFunc(fn), types...)
+}
+
+func (i *impl) RegisterStructValidation(fn interface{}, types ...interface{}) {
+	if fnConv, ok := fn.(v10.StructLevelFunc); !ok {
+		panic("fn is not v10.StructLevelFunc")
+	} else {
+		i.instance.RegisterStructValidation(fnConv, types...)
+	}
+}
--- validator/validator.go
+++ validator/validator.go
@@ -1,20 +1,15 @@
 package validator
 
-import (
-	"reflect"
-)
+import "reflect"
 
 type (
 	Validator interface {
+		Validate(obj interface{}) error
+		ValidateVar(obj interface{}, tag string) error
+
 		RegisterValidation(tag string, fn interface{})
-		RegisterAlias(alias string, tags string)
 		RegisterCustomTypeFunc(fn CustomTypeFunc, types ...interface{})
 		RegisterStructValidation(fn interface{}, types ...interface{})
-		InstallDefault()
-		Validate(object interface{}) error
-		AddTranslation(tag string, errorMsg string) error
-		RegisterTranslation(tag string, registerFn interface{}, transFn interface{}) error
-		TranslateError(err error) error
 	}
 
 	CustomTypeFunc func(field reflect.Value) interface{}
