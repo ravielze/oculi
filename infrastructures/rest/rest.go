a3dcd0af31860f45305b183aaa50f8f3f61b32ca
ravielze <ravielz20@gmail.com>
2021-07-21 02:43:58 +0700

    Add rest library

---
+++ infrastructures/rest/rest.go
@@ -0,0 +1,29 @@
+package rest
+
+import (
+	"reflect"
+
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	Registerable interface {
+		Register(ec *echo.Group) error
+	}
+)
+
+func Register(ec *echo.Group, obj interface{}) error {
+	v := reflect.ValueOf(obj)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		if r, ok := f.Interface().(Registerable); ok {
+			if err := r.Register(ec); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
