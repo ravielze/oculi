bd213745e7e231c3ae0f51d606829bd3a6a93f0a
ravielze <ravielz20@gmail.com>
2021-07-21 02:42:42 +0700

    Add health dto

---
+++ common/model/dto/health/check.go
@@ -0,0 +1,22 @@
+package health
+
+import "time"
+
+type (
+	CheckResponseDTO struct {
+		Ts     time.Time      `json:"ts"`
+		Pid    int            `json:"pid"`
+		Uptime time.Duration  `json:"uptime"`
+		Memory MemoryUsageDTO `json:"memory"`
+		Status string         `json:"status"`
+	}
+
+	MemoryUsageDTO struct {
+		Alloc      uint64 `json:"alloc"`
+		TotalAlloc uint64 `json:"totalAlloc"`
+		Sys        uint64 `json:"sys"`
+		HeapAlloc  uint64 `json:"heapAlloc"`
+		HeapSys    uint64 `json:"heapSys"`
+		NumGC      uint32 `json:"numGC"`
+	}
+)
