116eff7f79c91eec150e62d7137e9c9bc9ec4050
ravielze <ravielz20@gmail.com>
2021-07-25 02:15:19 +0700

    Add error transformer

--- context/statuscode.go
+++ context/statuscode.go
@@ -1,6 +1,10 @@
 package context
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/ravielze/oculi/errors"
+)
 
 func (ctx *Context) SetHttpCode(httpCode int) {
 	ctx.httpCode = httpCode
@@ -28,4 +32,12 @@ func (ctx *Context) HasError() bool {
 
 func (ctx *Context) Errors() []error {
 	return ctx.errors
+}
+
+func (ctx *Context) TransformError(errorMap errors.Mappers) {
+	if ctx.HasError() {
+		err := errors.Transform(ctx.errors[0], errorMap)
+		ctx.errors = append([]error{err}, ctx.errors...)
+		ctx.httpCode = err.Code
+	}
 }
