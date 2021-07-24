1c4171de35d1ca7950874968ed5d59bc86ff4e15
ravielze <ravielz20@gmail.com>
2021-07-24 16:46:34 +0700

    Edit middleware

--- middleware/token/echo.go
+++ middleware/token/echo.go
@@ -5,25 +5,24 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	consts "github.com/ravielze/oculi/constant/key"
 	"github.com/ravielze/oculi/context"
 	"github.com/ravielze/oculi/response"
 	"github.com/ravielze/oculi/token"
-)
-
-const (
-	CredentialsKey = "CREDENTIALS"
 )
 
 func EchoMiddleware(token token.Tokenizer, response response.Responder) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			ctx := c.(*context.Context)
-			claims, err := token.DecodeHttpRequest(ctx.Request())
-			if err != nil {
-				ctx.AddError(http.StatusUnauthorized, fmt.Errorf("unauthorized: %s", err.Error()))
-				return response.NewJSONResponse(ctx, nil, nil)
+			if ctx.NeedAuth() {
+				claims, err := token.DecodeHttpRequest(ctx.Request())
+				if err != nil {
+					ctx.AddError(http.StatusUnauthorized, fmt.Errorf("unauthorized: %s", err.Error()))
+					return response.NewJSONResponse(ctx, nil, nil)
+				}
+				ctx.Set(consts.KeyCredentials, claims.Credentials())
 			}
-			ctx.Set(CredentialsKey, claims.Credentials())
 			return next(c)
 		}
 	}
