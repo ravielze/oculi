05f2f64d582ebcef832112c9c34eb2abea26f7ab
ravielze <ravielz20@gmail.com>
2021-07-26 18:31:02 +0700

    Update middleware token

--- middleware/token/echo.go
+++ middleware/token/echo.go
@@ -1,12 +1,15 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/ravielze/oculi/common/model/dto/user"
 	consts "github.com/ravielze/oculi/constant/key"
 	"github.com/ravielze/oculi/context"
+	"github.com/ravielze/oculi/response"
 	"github.com/ravielze/oculi/token"
 )
 
@@ -24,10 +27,38 @@ func EchoMiddleware(token token.Tokenizer) echo.MiddlewareFunc {
 			}
 			claims, err := token.DecodeHttpRequest(ctx.Request())
 			if err != nil {
-				ctx.AddError(http.StatusUnauthorized, fmt.Errorf("unauthorized: %s", err.Error()))
+				ctx.Set(consts.KeyCredentials, user.CredentialsDTO{
+					ID:       0,
+					Metadata: fmt.Errorf("unauthorized: %s", err.Error()),
+				})
+			} else {
+				ctx.Set(consts.KeyCredentials, claims.Credentials())
+			}
+			return next(c)
+		}
+	}
+}
+
+func PrivateEndpoint(r response.Responder) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		if !tokenMiddlewareActivated {
+			return func(c echo.Context) error {
 				return next(c)
 			}
-			ctx.Set(consts.KeyCredentials, claims.Credentials())
+		}
+		return func(c echo.Context) error {
+			ctx := c.(*context.Context)
+			credentials := ctx.Get(consts.KeyCredentials)
+			if credentials == nil {
+				ctx.AddError(http.StatusUnauthorized, errors.New("unauthorized: credentials not found"))
+				return r.NewJSONResponse(ctx, nil, nil)
+			} else {
+				cdto := credentials.(user.CredentialsDTO)
+				if cdto.ID == 0 {
+					ctx.AddError(http.StatusUnauthorized, cdto.Metadata.(error))
+					return r.NewJSONResponse(ctx, nil, nil)
+				}
+			}
 			return next(c)
 		}
 	}
