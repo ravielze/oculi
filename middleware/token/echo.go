db747289fa026e9316c0b2919ece6e71aa60d425
ravielze <ravielz20@gmail.com>
2021-08-11 03:34:17 +0700

    Refactor middleware token

--- middleware/token/echo.go
+++ middleware/token/echo.go
@@ -21,55 +21,69 @@ var (
 	tokenMiddlewareActivated = false
 )
 
+func fetchAuthentication(cf ClaimFunction, next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx, ok := c.(*context.Context)
+		if !ok {
+			ctx = context.New(c)
+		}
+		claims, err := cf(ctx.Request())
+		if err != nil {
+			ctx.Set(consts.KeyCredentials, user.CredentialsDTO{
+				ID:       0,
+				Metadata: errorUtil.InjectDetails(errConsts.ErrUnauthorized, err.Error()),
+			})
+		} else {
+			ctx.Set(consts.KeyCredentials, claims.Credentials())
+		}
+		return next(c)
+	}
+}
+
 func EchoMiddleware(cf ClaimFunction) echo.MiddlewareFunc {
 	tokenMiddlewareActivated = true
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			ctx, ok := c.(*context.Context)
-			if !ok {
-				ctx = context.New(c)
+		return fetchAuthentication(cf, next)
+	}
+}
+
+func skip(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return next(c)
+	}
+}
+
+func sendErrorIfNeeded(next echo.HandlerFunc, r response.Responder) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.(*context.Context)
+		credentials := ctx.Get(consts.KeyCredentials)
+		if credentials == nil {
+			ctx.AddError(
+				http.StatusUnauthorized,
+				errorUtil.InjectDetails(
+					errConsts.ErrUnauthorized,
+					"unauthorized: credentials not found",
+				),
+			)
+			return r.NewJSONResponse(ctx, nil, nil)
+		} else {
+			cdto := credentials.(user.CredentialsDTO)
+			if cdto.ID == 0 {
+				ctx.AddError(http.StatusUnauthorized, cdto.Metadata.(error))
+				return r.NewJSONResponse(ctx, nil, nil)
 			}
-			claims, err := cf(ctx.Request())
-			if err != nil {
-				ctx.Set(consts.KeyCredentials, user.CredentialsDTO{
-					ID:       0,
-					Metadata: errorUtil.InjectDetails(errConsts.ErrUnauthorized, err.Error()),
-				})
-			} else {
-				ctx.Set(consts.KeyCredentials, claims.Credentials())
-			}
-			return next(c)
 		}
+		return next(c)
 	}
 }
 
 func PrivateEndpoint(r response.Responder) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
+
 		if !tokenMiddlewareActivated {
-			return func(c echo.Context) error {
-				return next(c)
-			}
+			return skip(next)
 		}
-		return func(c echo.Context) error {
-			ctx := c.(*context.Context)
-			credentials := ctx.Get(consts.KeyCredentials)
-			if credentials == nil {
-				ctx.AddError(
-					http.StatusUnauthorized,
-					errorUtil.InjectDetails(
-						errConsts.ErrUnauthorized,
-						"unauthorized: credentials not found",
-					),
-				)
-				return r.NewJSONResponse(ctx, nil, nil)
-			} else {
-				cdto := credentials.(user.CredentialsDTO)
-				if cdto.ID == 0 {
-					ctx.AddError(http.StatusUnauthorized, cdto.Metadata.(error))
-					return r.NewJSONResponse(ctx, nil, nil)
-				}
-			}
-			return next(c)
-		}
+
+		return sendErrorIfNeeded(next, r)
 	}
 }
