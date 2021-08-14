4d660d267b9be013f77697d32c3547cf9a6751ea
ravielze <ravielz20@gmail.com>
2021-08-15 01:54:04 +0700

    Add qrcode comment

--- generator/qrcode/errorCorrectionLevel.go
+++ generator/qrcode/errorCorrectionLevel.go
@@ -3,17 +3,19 @@ package qrcode
 import "github.com/boombuler/barcode/qr"
 
 type (
+	// The lower correction level, the less dense the qr code image is,
+	// which improves minimum printing size.
 	ErrorCorrectionLevel int
 )
 
 const (
-	// Recovers 7% of data
+	// Recovers 7% of data | Level L
 	LowCorrection ErrorCorrectionLevel = iota
-	// Recovers 15% of data
+	// Recovers 15% of data | Level M
 	MediumCorrection
-	// Recovers 25% of data
+	// Recovers 25% of data | Level Q
 	MediumHighCorrection
-	// Recovers 30% of data
+	// Recovers 30% of data | Level H
 	HighCorrection
 )
 
