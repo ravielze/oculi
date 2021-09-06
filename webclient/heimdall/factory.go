20cac7155e56a5c83e31c4e2027e4121b0af8904
ravielze <ravielz20@gmail.com>
2021-09-06 08:00:20 +0700

    Add webclient factory

--- webclient/heimdall/factory.go
+++ webclient/heimdall/factory.go
@@ -1,24 +1,31 @@
 package heimdall
 
-type (
-//clientFactory struct{}
+import (
+	"time"
+
+	"github.com/gojek/heimdall/httpclient"
+	"github.com/ravielze/oculi/webclient"
 )
 
-// func (cf *clientFactory) Create(timeout time.Duration) webclient.Client {
-// 	return &client{
-// 		Doer: *httpclient.NewClient(httpclient.WithHTTPTimeout(timeout)),
-// 	}
-// }
+type (
+	clientFactory struct{}
+)
 
-// func (cf *clientFactory) CreateWithRetry(timeout time.Duration, retryCount int) webclient.Client {
-// 	return &client{
-// 		Doer: *httpclient.NewClient(
-// 			httpclient.WithHTTPTimeout(timeout),
-// 			httpclient.WithRetryCount(retryCount),
-// 		),
-// 	}
-// }
+func (cf *clientFactory) Create(timeout time.Duration) webclient.Client {
+	return &client{
+		Heimdall: *httpclient.NewClient(httpclient.WithHTTPTimeout(timeout)),
+	}
+}
 
-// func NewClientFactory() webclient.Factory {
-// 	return &clientFactory{}
-// }
+func (cf *clientFactory) CreateWithRetry(timeout time.Duration, retryCount int) webclient.Client {
+	return &client{
+		Heimdall: *httpclient.NewClient(
+			httpclient.WithHTTPTimeout(timeout),
+			httpclient.WithRetryCount(retryCount),
+		),
+	}
+}
+
+func New() webclient.WebClient {
+	return &clientFactory{}
+}
